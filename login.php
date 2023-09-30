2f3a446fae712cc22acbcfbb4a28d41dd2649924
MIwong05 <rizwaniwong939@gmail.com>
2023-09-30 13:22:54 +0700

    first

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+
+$username=2345;
+$password=2345;
+
+if($username == 2345 && $password == 2345){
+    echo "Anda Berhasil Login";
+}else if($suername == 2345 && $password == 7878){
+    echo "Maaf Password Anda Salah";
+}else if($username == 4545  &&  $password == 12345){
+    echo "Maaf Username Anda Salah";
+}else{
+    echo "Mohon Maaf Username Dan Password Anda Salah";
+}
+?>
